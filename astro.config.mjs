89f401cd30af0fa525e2a547bd4bb1a07ea4ab74
Phi An <phian.14092004@gmail.com>
2025-12-27 18:39:58 +0700

    Add github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://truongphian.github.io',
+  base: '/blog_phian',
   integrations: [mdx(), sitemap()],
 
   vite: {
